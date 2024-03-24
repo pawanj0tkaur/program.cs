915b1bfcf0a29cfd34b4d87933e5739ad7ab58fd
Dell <Dell@DESKTOP-O7TQ14S>
2024-03-24 15:05:40 -0400

    car

---
+++ Car.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program.cs
+{
+    internal class Car
+    {
+        public class Car : Vehicle
+        {
+            private int numberOfDoors;
+
+            public Car(string model, string manufacturer, int year, double rentalPrice, int numberOfDoors)
+                : base(model, manufacturer, year, rentalPrice)
+            {
+                this.numberOfDoors = numberOfDoors;
+            }
+
+            public int GetNumberOfDoors() => numberOfDoors;
+            public void SetNumberOfDoors(int numberOfDoors) => this.numberOfDoors = numberOfDoors;
+
+            // Override DisplayDetails method to include car-specific details
+            public override void DisplayDetails()
+            {
+                base.DisplayDetails();
+                Console.WriteLine($"Number of Doors: {numberOfDoors}");
+            }
+        }
+    }
+}
