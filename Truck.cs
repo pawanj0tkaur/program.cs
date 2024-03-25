a1c01d95a8d37b5170bf1f4e82cd3c3333a30303
Dell <Dell@DESKTOP-O7TQ14S>
2024-03-24 20:59:51 -0400

    Truck.cs

---
+++ Truck.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program.cs
+{
+    internal class Truck
+    {
+        public class Truck : Vehicle
+        {
+            private double cargoCapacity;
+
+            public Truck(string model, string manufacturer, int year, double rentalPrice, double cargoCapacity)
+                : base(model, manufacturer, year, rentalPrice)
+            {
+                this.cargoCapacity = cargoCapacity;
+            }
+
+            public double GetCargoCapacity() => cargoCapacity;
+            public void SetCargoCapacity(double cargoCapacity) => this.cargoCapacity = cargoCapacity;
+
+            // Override DisplayDetails method to include truck-specific details
+            public override void DisplayDetails()
+            {
+                base.DisplayDetails();
+                Console.WriteLine($"Cargo Capacity: {cargoCapacity} tons");
+            }
+        }
+
+    }
+}
