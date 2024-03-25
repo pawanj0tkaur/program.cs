e28210fcf641eb8a8ade57c39aac54586d7cc8e0
Dell <Dell@DESKTOP-O7TQ14S>
2024-03-25 14:38:19 -0400

    Motorcycle.cs

---
+++ Motorcycle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program.cs
+{
+    internal class Motorcycle
+    {
+        public class Motorcycle : Vehicle
+        {
+            private string type;
+
+            public Motorcycle(string model, string manufacturer, int year, double rentalPrice, string type)
+                : base(model, manufacturer, year, rentalPrice)
+            {
+                this.type = type;
+            }
+
+            public string GetType() => type;
+            public void SetType(string type) => this.type = type;
+
+            // Override DisplayDetails method to include motorcycle-specific details
+            public override void DisplayDetails()
+            {
+                base.DisplayDetails();
+                Console.WriteLine($"Type: {type}");
+            }
+        }
+    }
+}
